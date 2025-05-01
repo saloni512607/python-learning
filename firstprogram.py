0e88df01492f5a674fbeeb1f99b5b287e1b8a53a
SALONI ARIVASTAVA <salony5126@gmail.com>
2025-05-01 13:59:13 +0530

    Add files via upload

---
+++ firstprogram.py
@@ -0,0 +1,8 @@
+#WAP to check if the number entered by the user is even or odd
+n=int(input("enter the number to be checked:"))
+if n%2 == 0:
+    print("the number is even")
+else:
+    print("the number is odd")
+
+
